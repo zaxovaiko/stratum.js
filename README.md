1bcea825205ff6e2f2441d21142ad2c0371d2e8d
Volodia <zaxovaiko@gmail.com>
2018-06-17 18:37:45 +0300

    Added readme.md. Changed default columns value.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Stratum.js — Masonry Grid Library
+
+### Table of contents
+
+ - [Future updates](https://github.com/zaxoavoki/stratum#future-updates)
+ - [Copyright and license](https://github.com/zaxoavoki/stratum#copyright-and-license)
+
+### Future updates
+ 
+ - Add padding and margin gap between columns;
+ - Remove [jQuery](https://jquery.com/) from production version of the file;
+ - Add older browsers' supports;
+ - Add this package to the [npm](https://www.npmjs.com/) catalog;
+
+### Copyright and license
+Code and documentation copyright 2018. Code released under the [ISC License](https://en.wikipedia.org/wiki/ISC_license).
--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
 
-let $ = require("jquery");          // Require jQuery
+let $ = require("jquery"); // Import jQuery
 
 $(document).ready(function () {
 
-    let cols = 3;                   // Count of the columns
-    let grid = $('[data-grid]');    // Get all grids
+    let grid = $('[data-grid]');     // Get all grids
+    let columns = grid.data('grid'); // Count of the columns
+
+    // Set default value if user does not set count of the columns
+    if (columns === undefined || !columns) {
+        columns = 3;
+    }
 
     grid.css({
         position: 'relative'
     });
 
-    let items = [];                 // Array with item's height and width
-    let top = 0,                    // Initial values
-        left = -100 / cols;         // Left and top positions
+    let items = [];            // Array with item's height and width
+    let top = 0,               // Initial values
+        left = -100 / columns; // Left and top positions
 
     //
     // Main cycle: there we will check current coordinates
     grid.children().each(function (i) {
 
-        i % cols === 0 ? left = 0 : left += 100 / cols; // Set left position for every nth block
+        i % columns === 0 ? left = 0 : left += 100 / columns; // Set left position for every nth block
 
         // Set width and left position the first
         $(this).css({
             position : 'absolute',
-            width : 100 / cols + '%',
+            width : 100 / columns + '%',
             left: left + '%'
         });
 
-        items.push($(this).outerHeight(true));              // Save height value in main array to check top positions
+        items.push($(this).outerHeight(true)); // Save height value in main array to check top positions
 
         // Set top position
-        if (items[i - cols] === undefined) {
+        if (items[i - columns] === undefined) {
             top = 0;
         } else {
             let itemNumber = i;
             top = 0;
-            while (itemNumber >= cols) {
-                top += items[itemNumber - cols];
-                itemNumber -= cols;
+            while (itemNumber >= columns) {
+                top += items[itemNumber - columns];
+                itemNumber -= columns;
             }
         }
 
